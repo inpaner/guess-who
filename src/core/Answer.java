917c02f3af05d245affe50711e87ce289673d5bf
Ivan Paner <ivan.paner@gmail.com>
2016-07-19 13:25:27 +0800

    Added Answer.getScore()

--- src/core/Answer.java
+++ src/core/Answer.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by Ivan Paner on 7/18/2016.
@@ -30,17 +31,38 @@ public class Answer {
 
 
     public static void main(String[] args) {
+        Answer.testScore();
+    }
+
+    static void testGet() {
         for (Answer answer : cache) {
             System.out.println(answer.answer);
         }
     }
 
+    static void testScore() {
+        double score = Answer.getScore("yes");
+        System.out.println(score);
+        score = Answer.getScore("eh");
+        System.out.println(score);
+    }
 
     Answer(String answer, double score) {
         this.answer = answer;
         this.score = score;
     }
 
+
+    public static double getScore(String answerStr) {
+        double score = 0;
+        Answer answerTemp = new Answer(answerStr.toLowerCase(), 0);
+        int index = cache.indexOf(answerTemp);
+        if (index >= 0) {
+            Answer answer = cache.get(index);
+            score = answer.score;
+        }
+        return score;
+    }
 
     private static void populateCache() {
         Connection conn = null;
@@ -64,4 +86,20 @@ public class Answer {
             DaoUtil.close(conn, ps, rs);
         }
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Answer)) return false;
+        Answer other = (Answer) o;
+        return Objects.equals(this.answer, other.answer);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.answer);
+    }
+
+
 }
